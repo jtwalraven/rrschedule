4272c52a1c2e11b5bd730a37ebee617532b57a7e
Jeffrey Walraven <jeffrey@ravenswall.net>
2017-11-24 14:23:47 -0500

    Changed process editable fields to inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProcessTableComponent } from './process-table/process-table.component';
@@ -16,6 +17,7 @@ import { TimeQuantumComponent } from './time-quantum/time-quantum.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     md.MatInputModule,
     md.MatCardModule,
     md.MatButtonModule,
